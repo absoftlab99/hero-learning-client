cb8f9cb29702ec64633107096f44cde25c8186a8
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-10-26 21:42:57 +0600

    login and error showng handeld smartly

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,64 +1,39 @@
-// import React, { useContext, useState } from 'react';
 import Form from 'react-bootstrap/Form';
-// import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { FaGoogle, FaGithub } from 'react-icons/fa';
 import { ButtonGroup, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-// import { AuthContext } from '../../contexts/UserContext';
-// import { GithubAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/UserContext';
+import { useState } from 'react';
 
 const Login = () => {
-    // const [error, setError] = useState('');
-    // const {popUpSignIn, popUpGit, signInWithPassword} = useContext(AuthContext);
-    // const provider = new GoogleAuthProvider();
-    // const gitProvider = new GithubAuthProvider();
-    // const navigate = useNavigate();
+    const [error, setError] = useState('');
+    const {signInUser, user, setUser} = useContext(AuthContext);
 
-    // const location = useLocation();
-    // const from = location.state?.from?.pathname || '/';
+    const signInHandler = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
 
-
-    // const popUpHandler = (event) =>{
-    //     event.preventDefault();
-    //     popUpSignIn(provider)
-    //     .then(result =>{
-    //         const user = result.user;
-    //         console.log(user);
-    //     })
-    //     .catch(error =>{
-    //         console.error(error);
-    //     })
-    // }
-    // const signInHandler = (event) =>{
-    //     event.preventDefault();
-    //     const form = event.target;
-    //     const email = form.email.value;
-    //     const password = form.password.value;
-    //     signInWithPassword(email, password)
-    //     .then(result =>{
-    //         const user = result.user;
-    //         navigate(from, {replace: true});
-    //         console.log(user);
-    //     })
-    //     .catch(error =>{
-    //         console.error(error);
-    //         setError(error.message);
-    //     })
-    // }
-    // const gitPopUpHandler = (event) =>{
-    //     event.preventDefault();
-    //     popUpGit(gitProvider)
-    //     .then(result =>{
-    //         const user = result.user;
-    //         console.log(user);
-    //     })
-    //     .catch(error =>{
-    //         console.error(error);
-    //     })
-    // }
+        signInUser(email, password)
+        .then(result =>{
+            const user = result.user;
+            setUser(user)
+            setError('');
+            console.log(user);
+            console.log('login successfully');
+            form.reset();
+        })
+        .catch(error =>{
+            console.error(error);
+            setError(error.message);
+        })
+    }
 
     return (
-        <Form className='w-25 mx-auto border border-success p-4 rounded mt-5 text-start'>
+        <div className='w-25 mx-auto border border-success p-4 rounded mt-5 text-start'>
+            <Form onSubmit={signInHandler}>
             <h5>Login Form</h5>
             <hr></hr>
             <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -70,20 +45,25 @@ const Login = () => {
                 <Form.Label>Password</Form.Label>
                 <Form.Control className='border-success border-opacity-50' name='password' type="password" placeholder="Password" />
             </Form.Group>
-            {/* {
-                error?
+            {
+                error ?
                 <p className='alert alert-danger'>{error}</p> :
                 ''
-            } */}
+            }
+            {
+                !error && user ? <p className='alert alert-success'>Login Successfully</p> :
+                ''
+            }
             <Button className='w-100 mb-3' variant="outline-success" type="submit">
                 Login
             </Button><br></br>
-            <div className='text-center'><small>New to Hero Learning? <Link to='/register'>Create Account</Link></small></div>
-            <ButtonGroup vertical className='w-100 mt-2'>
-                <Button variant="outline-danger" className='mb-2'><FaGoogle></FaGoogle> Login With Google</Button>
-                <Button variant="outline-dark"><FaGithub></FaGithub> Login With Github</Button>
-            </ButtonGroup>
         </Form>
+        <div className='text-center'><small>New to Hero Learning? <Link to='/register'>Create Account</Link></small></div>
+        <ButtonGroup vertical className='w-100 mt-2'>
+            <Button variant="outline-danger" className='mb-2'><FaGoogle></FaGoogle> Login With Google</Button>
+            <Button variant="outline-dark"><FaGithub></FaGithub> Login With Github</Button>
+        </ButtonGroup>
+        </div>
     );
 };
 
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import app from '../firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { createContext } from 'react';
+import { useState } from 'react';
 ;
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({children}) => {
-
+    const [user, setUser] = useState(null);
     const createUser = (email, password)=>{
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
-    const authInfo = {createUser}
+    const signInUser = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = {createUser, signInUser, user, setUser}
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
